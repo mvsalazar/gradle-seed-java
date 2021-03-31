8da9960cf9855821e4541ffd73d0c6279ca83778
Mel Salazar <melvin.v.salazar@gmail.com>
2021-03-30 17:22:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# PlayFramework with Gradle support for VSCode | Eclipse
+
+A [Play Framework!](https://www.playframework.com/) Java seed with Gradle support. Out of the box works in Mac/Linux builds as well as Windows. 
+
+## Build
+### Mac/Linux
+
+```
+./gradlew build
+```
+
+### Windows
+```
+gradlew.bat build
+```
+
+## IDE - VSCODE | ECLIPSE
+```
+./gradlew eclipse
+```
---
+++ build/src/play/twirl/views/html/index.template.scala
@@ -0,0 +1,53 @@
+
+package views.html
+
+import _root_.play.twirl.api.TwirlFeatureImports._
+import _root_.play.twirl.api.TwirlHelperImports._
+import _root_.play.twirl.api.Html
+import _root_.play.twirl.api.JavaScript
+import _root_.play.twirl.api.Txt
+import _root_.play.twirl.api.Xml
+import models._
+import controllers._
+import play.api.i18n._
+import play.api.templates.PlayMagic._
+import play.api.mvc._
+import play.api.data._
+import views.html._
+
+object index extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
+
+  /**/
+  def apply/*1.2*/():play.twirl.api.HtmlFormat.Appendable = {
+    _display_ {
+      {
+
+
+Seq[Any](format.raw/*2.1*/("""
+"""),_display_(/*3.2*/main("Welcome to Play")/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
+  """),format.raw/*4.3*/("""<h1>Welcome to Play!</h1>
+""")))}),format.raw/*5.2*/("""
+"""))
+      }
+    }
+  }
+
+  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
+
+  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
+
+  def ref: this.type = this
+
+}
+
+
+              /*
+                  -- GENERATED --
+                  DATE: 2021-03-30T17:00:22.254931
+                  SOURCE: /home/msalazar/Documents/Code/gradle-seed-java/app/views/index.scala.html
+                  HASH: 479cb5189a226e8645768c0fb5cf32d29854fa10
+                  MATRIX: 722->1|818->4|845->6|876->29|915->31|944->34|1000->61
+                  LINES: 21->1|26->2|27->3|27->3|27->3|28->4|29->5
+                  -- GENERATED --
+              */
+          
